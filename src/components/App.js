d6352fb3f31715bcda23fcc7c06970cfd3b0dec1
Mr-R-e-X <souvikhazra151@gmail.com>
2024-06-28 11:37:45 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./../styles/App.css";
-import Child from "./Child.js";
+import ParentComponent from "./ParentComponent";
 const App = () => {
-  const [selectedOption, setSelectedOption] = useState("");
+  
   return (
-    <div>
-      <div className="parent">
-        {["Child Component 1", "Child Component 2"].map((option, idx) => (
-          <Child
-            key={idx}
-            text={option}
-            setOption={setSelectedOption}
-            count={idx + 1}
-          />
-        ))}
-      </div>
-      <p>Selected Option: {selectedOption}</p>
-    </div>
+      <ParentComponent/>
+      
   );
 };
 
