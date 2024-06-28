f68dedeb9ded7b06ad2df87cd8a5b35357bf049f
Mr-R-e-X <souvikhazra151@gmail.com>
2024-06-28 11:31:03 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
-import React from "react";
-import './../styles/App.css';
-
+import React, { useState } from "react";
+import "./../styles/App.css";
+import Child from "./Child.js";
 const App = () => {
+  const [selectedOption, setSelectedOption] = useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
+      <div className="parent">
+        {["Child Component 1", "Child Component 2"].map((option, idx) => (
+          <Child
+            key={idx}
+            text={option}
+            setOption={setSelectedOption}
+            count={idx + 1}
+          />
+        ))}
+      </div>
+      <p>Selected Option: {selectedOption}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
